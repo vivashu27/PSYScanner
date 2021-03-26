3c1474ea02f77513914b6c62deaf4138531075b1
luffy27 <52876924+vivashu27@users.noreply.github.com>
2021-03-26 18:00:45 +0530

    Update psyscanner.py

--- psyscanner.py
+++ psyscanner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.7
+
 import socket
 import sys
 import getopt
@@ -6,13 +8,16 @@ from queue import Queue
 from termcolor import colored
 threading.Lock()
 q=Queue()
-
+portopen=list()
+socket.setdefaulttimeout(5)
 def usage():
     print colored("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n","red")
-    print colored("\n\t\t\tPSYSCANNER !!! \n\n","red")
+    print colored("\n\t\t\tPSYSCANNER !!! v1.1\n\n","red")
+    print colored("\t\t\tby @luffy27\n\n","red")
     print colored("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n","red")
     print colored("-t --target for host\n","red")
     print colored("-r --range 0 100 for port range\n","red")
+    print colored("-b --banner to grap services on ports\n","red")
     print colored("-h --help\n","red")
     print colored("Use ./scannme.py -t 127.0.0.1 -r 80 2000 -h\n","red")
     
@@ -22,6 +27,7 @@ def main():
     global port1
     global port2
     global opt
+    ban=False
     opt=""
     host=""
     port1=0
@@ -29,7 +35,7 @@ def main():
     if not len(sys.argv[1:]):
         usage()
     try:
-        opt,arg=getopt.getopt(sys.argv[1:],'t:r:h',["target","range","help"])
+        opt,arg=getopt.getopt(sys.argv[1:],'t:r:b,h',["target","range","banner","help"])
     except:
         print("some thing occured")
 
@@ -41,6 +47,8 @@ def main():
         elif o in ("-r","--range"):
             port1=int(a)
             port2=int(sys.argv[-1])
+        elif o in ("-b","--banner"):
+            ban=True
             
     for j in range(port1,port2+1):
         q.put(j)
@@ -52,19 +60,32 @@ def main():
         thread=threading.Thread(target=scan,args=(host,port1))
         thread.daemon=True
         thread.start()
-       
+    if ban:
+        banner(host)
 
 def scan(host,p1):
-    
     while not q.empty():
         sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         port=q.get()
         res=sock.connect_ex((host,port))
         if res==0:
-            print colored("Port OPEN: ","green"),colored(port,"green")
+            portopen.append(int(port))
+            print colored("OPEN: ","green"),colored(str(port)+"\n","green")
         if q.empty():
-            sock.close()
-            sys.exit()
-    
+            break
+
+def banner(host):
+    print(portopen)
+    for ports in portopen:
+        try:
+            socks=socket.socket()
+            socks.connect((host,ports))
+            socks.send("hello\n\t")
+            banner=socks.recv(95)
+            print colored("Port: ","green"),colored(str(ports),"green"),colored("banner: "+banner+"\n","green") 
+        except Exception as e:
+            print colored("Port: ","green"),colored(str(ports),"green")
+        
+        
         
 main()
